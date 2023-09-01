52e7882ca0afc2f7dbe59ce09f50b79bb9209a39
Suresh Krishna <krishnapaulraj2004@gmail.com>
2023-09-01 20:18:45 +0530

    data from cookies

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -2,36 +2,38 @@
 
 import axios from "axios";
 import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function page() {
   const router = useRouter();
 
-  const [data, setData] = useState();
+  const [data, setData] = useState("Hello");
 
   const logOut = async () => {
     await axios.get("/api/users/logout");
     router.push("/signup");
   };
 
-  const getData = async () => {
-    const res: any = await axios.get("/api/users/data");
-    setData(res.data.data.name);
-  };
+  useEffect(() => {
+    const getData = async () => {
+      const res: any = await axios.get("/api/users/data");
+      setData("Hello " + res.data.data.name);
+    };
+    getData();
+  }, []);
 
   return (
-    <main className="h-screen">
+    <main className="flex flex-col justify-center items-center h-screen">
       <div className="flex flex-col justify-center items-center gap-5">
         <h1 className="text-xl">Welcome Page</h1>
+        <h1 className="text-white text-xl">{data}</h1>
         <button
-          className="bg-slate-500 text-white p-2 rounded-md"
+          className="bg-slate-500 text-white p-2 rounded-md hover:bg-slate-400"
           onClick={logOut}
           type="button"
         >
           Logout
         </button>
-        <button onClick={getData}>Get Data</button>
-        <h1 className="text-white">{data}</h1>
       </div>
     </main>
   );
