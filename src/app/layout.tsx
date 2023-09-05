bfab299db3763038c598f68b92700c73ab63521a
Suresh Krishna <krishnapaulraj2004@gmail.com>
2023-09-05 20:35:59 +0530

    bug

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-slate-400/50">
+      <body className="">
         <Navbar /> {children}
       </body>
     </html>
